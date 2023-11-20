801b183be3a3834b9e420e9df409a9d71bf6e381
cqrita <52904285+cqrita@users.noreply.github.com>
2023-11-20 17:30:56 +0900

    commit

---
+++ JSON/20231114_01/20231114_01.cpp
@@ -0,0 +1,37 @@
+﻿// rapidjson/example/simpledom/simpledom.cpp`
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/filereadstream.h"
+#include <iostream>
+#include "Utility.h"
+using namespace rapidjson;
+using namespace std;
+struct Player
+{
+    int hp;
+    int level;
+};
+int main() {
+    errno_t err;
+    FILE* fp;
+    // 1. Parse a JSON string into DOM.
+    err = fopen_s(&fp,"myData.json", "rb"); // non-Windows use "r"
+
+    char readBuffer[16384];
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    Document d;
+    d.ParseStream(is);
+
+    // 2. Modify it by DOM.
+    Value& hp = d["hp"];
+    Player p;
+    p.hp = hp.GetInt();
+    Value& level = d["hp"];
+    p.level = level.GetInt();
+    cout << "hp :" << p.hp << endl;
+    cout << "level :" << p.level << endl;
+    err = fclose(fp);
+
+    return 0;
+}
